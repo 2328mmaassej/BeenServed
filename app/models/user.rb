c8aa84f8f6ef7fd6c0b9acca5a7acae45e837301
Lowell <lowell@oobeyagroup.com>
2013-03-02 15:52:52 -0600

    User pages updated and sessions controller created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :username
+  attr_accessible :email, :password, :username, :password_confirmation
+
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email
+
   has_many :meals
   has_many :restaurants, through: :meals
   has_many :dishes, through: :meals
+
+  has_secure_password
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,17 @@ BeenServed::Application.routes.draw do
   resources :restaurants
 
 
-  resources :users
+  get '/users' => 'users#index', as: :users
+  post '/users' => 'users#create'
+  get '/signup' => 'users#new', as: :signup
+  get '/users/:id/edit' => 'users#edit', as: :edit_user
+  get '/users/:id' => 'users#show', as: :user
+  put '/users/:id' => 'users#update'
+  delete '/users/:id' => 'users#destroy'
+
+  get '/login' => 'sessions#new', as: :login
+  post '/sessions/create' => 'sessions#create', as: :create_session
+  get '/logout' => 'sessions#destroy', as: :logout
 
 
   # The priority is based upon order of creation:
