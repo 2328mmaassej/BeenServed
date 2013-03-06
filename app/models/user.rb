6c269876a6b2624a4e71a7e9b12744207e5a4e1d
Amanda Griffin-Johnson <amanda.griffin@gmail.com>
2013-03-05 21:28:09 -0600

    Updated user profile and meal creation form. Note: Picture upload not yet functional.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :username, :password_confirmation, :password_digest
 
-  validates_presence_of :username, :email
+  validates_presence_of :username, :email, :password
   validates_uniqueness_of :username, :email
 
   has_many :meals
